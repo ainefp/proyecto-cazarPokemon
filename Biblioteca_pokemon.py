df1b525455b04024ed9428ef77c0a6ee634d7c26
Ainé Figueroa <aine.xena.100@gmail.com>
2025-01-20 15:14:17 +0100

    biblioreca aun no funciona

--- Biblioteca_pokemon.py
+++ Biblioteca_pokemon.py
@@ -5,21 +5,29 @@ class Biblioteca:
     pokemon: Pokemon
     lista: list
 
-    def __init__(self, pokemon):
+    def __init__(self, pokemon: Pokemon):
         self.pokemon = pokemon
         self.lista = []
     
-    def agregar_pokemon(self):
+    def agregar_pokemon(self) -> list:
         self.lista.append(self.pokemon)
+        return self.lista
     
     def __str__(self):
         lista = "".join(self.lista)
         print(lista)
 
 ponyta = Pokemon("ponyta", contar_letras("ponyta"), "fuego", 24.5, 1.12)
-Biblioteca(ponyta)
-
-
-
 print(ponyta)
-print(Biblioteca(ponyta))
+
+ponyta = Biblioteca(ponyta)
+ponyta.agregar_pokemon()
+
+
+pikachu = Pokemon("pikachu", contar_letras("pikachu"), "electrico", 16.6, 0.98)
+print(pikachu)
+
+pikachu = Biblioteca(pikachu)
+pikachu.agregar_pokemon()
+
+print(pikachu)
