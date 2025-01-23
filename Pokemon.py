e7866d9bf7d0f6e8051c749699c2c9ad901fc071
Ainé Figueroa <aine.xena.100@gmail.com>
2025-01-23 13:47:26 +0100

    cambiado 'ctm' por 'metros'

--- Pokemon.py
+++ Pokemon.py
@@ -1,9 +1,11 @@
+from contador_letras import contar_letras
+
 class Pokemon:
     nombre: str
     cantidadLetras: int
     tipo: str
     peso: float  # en kg
-    tamanho: float  # en ctm
+    tamanho: float  # en metros
 
     def __init__(self, nombre, cantidadLetras, tipo, peso, tamanho):
         self.nombre = nombre
@@ -15,3 +17,6 @@ class Pokemon:
     def __str__(self):
         return f"El pokemon {self.nombre} es de tipo {self.tipo}, pesa {self.peso}kg y mide {self.tamanho}m."
     
+
+ponyta = Pokemon("ponyta", contar_letras("ponyta"), "fuego", 24.5, 1.12)
+print(ponyta)
