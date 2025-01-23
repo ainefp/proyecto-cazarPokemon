5fd187c79b7da4975b556a4915833310bddf8f5b
Ainé Figueroa <aine.xena.100@gmail.com>
2025-01-23 14:20:46 +0100

    añadido typehint

--- contador_letras.py
+++ contador_letras.py
@@ -1,4 +1,4 @@
-def contar_letras(nombre):
+def contar_letras(nombre: str) -> int:
     """Función utilizada para contar los caracteres de la cadena que se le pase.
     Se utilizará para contar las letras de los nombres de pokemons, por ello se llama 'contar_letras'.
 
