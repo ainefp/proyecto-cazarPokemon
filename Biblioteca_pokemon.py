7d870cce25cd8c4eb8cbc71e7546df603ba823bf
Ainé Figueroa <aine.xena.100@gmail.com>
2025-01-23 14:15:19 +0100

    BIBLIOTECA FUNCIONA

--- Biblioteca_pokemon.py
+++ Biblioteca_pokemon.py
@@ -1,36 +1,29 @@
 from Pokemon import Pokemon
-from contador_letras import contar_letras
+from Contador_letras import contar_letras
 
 # La clase Biblioteca se utiliza para guardar los pokemons del objeto Pokemon
 class Biblioteca:
-    pokemon: Pokemon
-    lista: list = []
+    lista: list[Pokemon]
 
-    def __init__(self, pokemon: Pokemon):
-        self.pokemon = pokemon
-        self.lista = Biblioteca.agregar_pokemon(pokemon)
+    def __init__(self):
+        self.lista = []
     
-    def agregar_pokemon(pokemon: Pokemon) -> list:
-        Biblioteca.lista.append(pokemon)
-    
-    def ver_biblioteca(self):
-        lista = "".join(self.lista)
-        print(lista)
-    
+    def agregar_pokemon(self, pokemon: Pokemon):
+        self.lista.append(pokemon)
+
     def __str__(self):
-        return Pokemon(self.pokemon)
+        resultado = ""
+        for pokemon in self.lista:
+            resultado += f"{str(pokemon)}\n"
+        return resultado
         
+if __name__ == "__main__":
+    biblioteca = Biblioteca()
 
-ponyta = Pokemon("ponyta", contar_letras("ponyta"), "fuego", 24.5, 1.12)
-ponyta2 = Biblioteca(ponyta)
-ponyta2.agregar_pokemon()
-#ponyta2.ver_biblioteca()
-# print(ponyta)
-# print(ponyta2)
+    ponyta = Pokemon("ponyta", contar_letras("ponyta"), "fuego", 24.5, 1.12)
+    pikachu = Pokemon("pikachu", contar_letras("pikachu"), "electrico", 16.6, 0.98)
 
+    biblioteca.agregar_pokemon(ponyta)
+    biblioteca.agregar_pokemon(pikachu)
 
-pikachu = Pokemon("pikachu", contar_letras("pikachu"), "electrico", 16.6, 0.98)
-print(pikachu)
-
-pikachu = Biblioteca(pikachu)
-pikachu.agregar_pokemon()
+    print(biblioteca)
