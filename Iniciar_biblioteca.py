eb73b547a67f9a082e8b466332cf66701a5c7ee2
Ainé Figueroa <aine.xena.100@gmail.com>
2025-01-24 13:03:56 +0100

    creados archivos Inicar_biblioteca y Pokedex

---
+++ Iniciar_biblioteca.py
@@ -0,0 +1,1 @@
+# Aquí se agregan a los pokemons
---
+++ Pokedex.py
@@ -0,0 +1,2 @@
+# Aquí se crea la clase pokedex, donde los pokemons se van guardando conforme se van acertando sus nombres.
+# Aún no sé si hacerlo con listas o con diccionarios.
