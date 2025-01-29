331fe38d99a272d9e0fc7363badbe6cdc1bdfdc7
Ainé Figueroa <aine.xena.100@gmail.com>
2025-01-29 12:48:55 +0100

    creado .json para pasar los datos de 'iniciar_biblioteca'

--- Iniciar_biblioteca.py
+++ Iniciar_biblioteca.py
@@ -6,52 +6,52 @@ from Biblioteca_pokemon import Biblioteca
 # lista: https://pokemon.fandom.com/es/wiki/Lista_de_Pok%C3%A9mon_seg%C3%BAn_la_Pok%C3%A9dex_de_Kanto
 biblioteca = Biblioteca()
 
-bulbasaur = Pokemon("bulbasaur", contar_letras("bulbasaur"), "planta/veneno", 0, 0, 1)
+bulbasaur = Pokemon("bulbasaur", contar_letras("bulbasaur"), "planta/veneno", 0.7, 6.9, 1)
 biblioteca.agregar_pokemon(bulbasaur)
 
-ivysaur = Pokemon("ivysaur", contar_letras("ivysaur"), "planta/veneno", 0, 0, 2)
+ivysaur = Pokemon("ivysaur", contar_letras("ivysaur"), "planta/veneno", 1, 13, 2)
 biblioteca.agregar_pokemon(ivysaur)
 
-venusaur = Pokemon("venusaur", contar_letras("venusaur"), "planta/veneno", 0, 0, 3)
+venusaur = Pokemon("venusaur", contar_letras("venusaur"), "planta/veneno", 2.4, 155.5, 3)
 biblioteca.agregar_pokemon(venusaur)
 
-charmander = Pokemon("charmander", contar_letras("charmander"), "fuego", 0, 0, 4)
+charmander = Pokemon("charmander", contar_letras("charmander"), "fuego", 0.6, 8.5, 4)
 biblioteca.agregar_pokemon(charmander)
 
-charmeleon = Pokemon("charmeleon", contar_letras("charmeleon"), "fuego", 0, 0, 5)
+charmeleon = Pokemon("charmeleon", contar_letras("charmeleon"), "fuego", 1.1, 19, 5)
 biblioteca.agregar_pokemon(charmeleon)
 
-charizard = Pokemon("charizard", contar_letras("charizard"), "fuego/volador", 0, 0, 6)
+charizard = Pokemon("charizard", contar_letras("charizard"), "fuego/volador", 1.7, 90.5, 6)
 biblioteca.agregar_pokemon(charizard)
 
-squirtle = Pokemon("squirtle", contar_letras("squirtle"), "agua", 0, 0, 7)
+squirtle = Pokemon("squirtle", contar_letras("squirtle"), "agua", 0.5, 9, 7)
 biblioteca.agregar_pokemon(squirtle)
 
-wartortle = Pokemon("wartortle", contar_letras("wartortle", "agua"), 0, 0, 8)
+wartortle = Pokemon("wartortle", contar_letras("wartortle"), "agua", 1, 22.5, 8)
 biblioteca.agregar_pokemon(wartortle)
 
-blastoise = Pokemon("blastoise", contar_letras("blastoise"), "agua", 0, 0, 9)
+blastoise = Pokemon("blastoise", contar_letras("blastoise"), "agua", 1.6, 85.5, 9)
 biblioteca.agregar_pokemon(blastoise)
 
-caterpie = Pokemon("caterpie", contar_letras("caterpie"), "bicho", 0, 0, 10)
+caterpie = Pokemon("caterpie", contar_letras("caterpie"), "bicho", 0.3, 2.9, 10)
 biblioteca.agregar_pokemon(caterpie)
 
-metapod = Pokemon("metapod", contar_letras("metapod"), "bicho", 0, 0, 11)
+metapod = Pokemon("metapod", contar_letras("metapod"), "bicho", .7, 9.9, 11)
 biblioteca.agregar_pokemon(metapod)
 
-butterfree = Pokemon("butterfree", contar_letras("butterfree"), "bicho/volador", 0, 0, 12)
+butterfree = Pokemon("butterfree", contar_letras("butterfree"), "bicho/volador", 1.1, 32, 12)
 biblioteca.agregar_pokemon(butterfree)
 
-weedle = Pokemon("weedle", contar_letras("weedle"), "bicho/veneno", 0, 0, 13)
+weedle = Pokemon("weedle", contar_letras("weedle"), "bicho/veneno", 0.3, 3.2, 13)
 biblioteca.agregar_pokemon(weedle)
 
-kakuna = Pokemon("kakuna", contar_letras("kakuna"), "bicho/veneno", 0, 0, 14)
+kakuna = Pokemon("kakuna", contar_letras("kakuna"), "bicho/veneno", 0.6, 10, 14)
 biblioteca.agregar_pokemon(kakuna)
 
-beedril = Pokemon("beedril", contar_letras("beedril", "bicho/veneno"), 0, 0, 15)
+beedril = Pokemon("beedril", contar_letras("beedril"), "bicho/veneno", 1, 29.5, 15)
 biblioteca.agregar_pokemon(beedril)
 
-pidgey = Pokemon("pidgey", contar_letras("pidgey"), "normal/volador", 0, 0, 16)
+pidgey = Pokemon("pidgey", contar_letras("pidgey"), "normal/volador", 0.3, 1.8, 16)
 biblioteca.agregar_pokemon(pidgey)
 
 pidgeotto = Pokemon("pidgeotto", contar_letras("pidgeotto"), "normal/volador", 0, 0, 17)
@@ -90,13 +90,22 @@ biblioteca.agregar_pokemon(sandshrew)
 sandslash = Pokemon("sandslash", contar_letras("sandslash"), "tierra", 0, 0, 28)
 biblioteca.agregar_pokemon(sandslash)
 
-nidoran = Pokemon("nidoran", contar_letras("nidoran"), "veneno", 0, 0, 29)
-biblioteca.agregar_pokemon(nidoran)
+nidoran_h = Pokemon("nidoran_h", contar_letras("nidoran_h"), "veneno", 0, 0, 29)
+biblioteca.agregar_pokemon(nidoran_h)
 
-nidorina tierra
+nidorina = Pokemon("nidorina", contar_letras("nidorina"), "tierra", 0, 0, 30)
+biblioteca.agregar_pokemon(nidorina)
 
-nidoqueen veneno/tierra
+nidoqueen = Pokemon("nidoqueen", contar_letras("nidoqueen"), "veneno/tierra", 0, 0, 31)
+biblioteca.agregar_pokemon(nidoqueen)
 
-nidoran tierra
-nidorino tierra
-nidoking veneno/tierra
+nidoran_m = Pokemon("nidoran_m", contar_letras("nidoran_m"), "tierra", 0, 0, 32)
+biblioteca.agregar_pokemon(nidoran_m)
+
+nidorino = Pokemon("nidorino", contar_letras("nidorino"), "tierra", 0, 0, 33)
+biblioteca.agregar_pokemon(nidorino)
+
+nidoking = Pokemon("nidoking", contar_letras("nidoking"), "veneno/tierra", 0, 0, 34)
+biblioteca.agregar_pokemon(nidoking)
+
+print(biblioteca)
--- Pokemon.py
+++ Pokemon.py
@@ -2,17 +2,17 @@ class Pokemon:
     nombre: str
     cantidadLetras: int
     tipo: str
-    peso: float  # en kg
     tamanho: float  # en metros
+    peso: float  # en kg
     n_pokedex: int
 
-    def __init__(self, nombre, cantidadLetras, tipo, peso, tamanho, n_pokedex):
+    def __init__(self, nombre, cantidadLetras, tipo, tamanho, peso, n_pokedex):
         self.nombre = nombre
         self.cantidadLetras = cantidadLetras
         self.tipo = tipo
-        self.peso = peso
         self.tamanho = tamanho
+        self.peso = peso
         self.n_pokedex = n_pokedex
 
     def __str__(self):
-        return f"El pokemon {self.nombre} es de tipo {self.tipo}, pesa {self.peso}kg, mide {self.tamanho}m y su número en la pokedex es {self.n_pokedex}."
+        return f"El pokemon {self.nombre} es de tipo {self.tipo}, mide {self.tamanho}m, pesa {self.peso}kg y su número en la pokedex es {self.n_pokedex}."
