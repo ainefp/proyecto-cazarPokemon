fcd2357cf1dc446429f5e3a4e4f470f0404cee18
Ainé Figueroa <aine.xena.100@gmail.com>
2025-02-05 11:09:29 +0100

    eliminadas las pruebas en los ficheros

--- Biblioteca_pokemon.py
+++ Biblioteca_pokemon.py
@@ -16,14 +16,3 @@ class Biblioteca:
         for pokemon in self.lista:
             resultado += f"{str(pokemon)}\n"
         return resultado
-        
-if __name__ == "__main__":
-    biblioteca = Biblioteca()
-
-    ponyta = Pokemon("ponyta", contar_letras("ponyta"), "fuego", 24.5, 1.12, 77)
-    pikachu = Pokemon("pikachu", contar_letras("pikachu"), "electrico", 16.6, 0.98, 25)
-
-    biblioteca.agregar_pokemon(ponyta)
-    biblioteca.agregar_pokemon(pikachu)
-
-    print(biblioteca)
