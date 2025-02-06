502b48be9e71d3c3e99f82f6485f4b9b97646f5d
Ainé Figueroa <aine.xena.100@gmail.com>
2025-02-06 13:46:34 +0100

    bienvenida y menu de opciones

---
+++ Vista.py
@@ -0,0 +1,24 @@
+class Vista:
+    SALIR = 4
+
+    def bienvenida(self) -> None:
+        print("======================================")
+        print("  Bienvenid@ al juego Cazar Pokemons")
+        print("======================================")
+    
+    def mostrar_menu_opcion(self) -> None:
+        print("\n1. Adivinar letra")
+        print("2. Adivinar palabra")
+        print("3. Pedir pista")
+        print("4. Me rindo! ¿Cuál es el pokemon?\n")
+    
+    def pedir_letra(self):
+        return input("Introduce la letra: ")
+    
+    def mostrar_pista(self):
+        pass
+
+if __name__ == "__main__":
+    vista = Vista()
+    print(vista.bienvenida())
+    print(vista.mostrar_menu_opcion())
