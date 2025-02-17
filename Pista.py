df0c6e48e144105384ca7fceac27cc2c63dadb7b
Ainé <aine.xena.100@gmail.com>
2025-02-17 12:18:44 +0000

    elección aleatoria de pistas hecha

--- Pista.py
+++ Pista.py
@@ -1,20 +1,31 @@
 from Biblioteca import Biblioteca
 from Pokemon import Pokemon
 from Contador_letras import contar_letras
-from random import randint
+import random
 import json
 
 class Pista:
     pistas: list
 
     def __init__(self):
-        self.pistas = []
+        with open('Generar_pokemon.json', 'r') as archivo:
+            # Cargar los datos en un diccionario
+            pokemons = json.load(archivo)
+    
+        # Elegir una clave aleatoria del diccionario
+        clave_aleatoria = random.choice(list(pokemons.keys()))
 
-    def generar_pista(self):
-        pass
+        # Cargar los datos del Pokémon aleatorio en una variable
+        pokemon_aleatorio = pokemons[clave_aleatoria]
 
-    def pedir_pista(self):
-        pass
+        self.pistas = [f"El pokemon es de tipo {pokemon_aleatorio['tipo']}",
+                       f"El pokemon pesa {pokemon_aleatorio['peso']} kg",
+                       f"El pokemon mide {pokemon_aleatorio['tamanho']} m",
+                       f"El número de pokedex del pokemon es {pokemon_aleatorio['n_pokedex']}"]
+
+    def generar_pista(self, n_frase: int) -> str:
+        resultado = self.pistas[n_frase]
+        return resultado
 
 if __name__ == "__main__":
     biblioteca = Biblioteca()
@@ -31,3 +42,14 @@ if __name__ == "__main__":
     for pokemon in pokemons:
         datos = pokemons[pokemon]
         print(f"Nombre: {datos['nombre']}\nTipo: {datos['tipo']}\nPeso: {datos['peso']}\n")
+    
+    # Elegir una clave aleatoria
+    clave_aleatoria = random.choice(list(pokemons.keys()))
+
+    # Obtener los datos del Pokémon aleatorio
+    pokemon_aleatorio = pokemons[clave_aleatoria]
+    
+    print(f"¡Has encontrado a {pokemon_aleatorio['nombre']}!")
+
+    n_pista = random.randint(0, 3)
+    print(pista.generar_pista(n_pista))
