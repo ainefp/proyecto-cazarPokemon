534e380c01720e25f117211675ba96f2c82c31c9
Ainé <aine.xena.100@gmail.com>
2025-02-17 12:32:56 +0000

    eliminadas pruebas de pista

--- Pista.py
+++ Pista.py
@@ -1,6 +1,3 @@
-from Biblioteca import Biblioteca
-from Pokemon import Pokemon
-from Contador_letras import contar_letras
 import random
 import json
 
@@ -26,30 +23,3 @@ class Pista:
     def generar_pista(self, n_frase: int) -> str:
         resultado = self.pistas[n_frase]
         return resultado
-
-if __name__ == "__main__":
-    biblioteca = Biblioteca()
-    pikachu = Pokemon("pikachu", contar_letras("pikachu"), "electro", 1.5, 23, 46)
-    biblioteca.agregar_pokemon(pikachu)
-    pista = Pista()
-
-    # Abrir el json
-    with open('Generar_pokemon.json', 'r') as archivo:
-        # Cargar los datos en un diccionario
-        pokemons = json.load(archivo)
-
-    # Acceder a dichos datos
-    for pokemon in pokemons:
-        datos = pokemons[pokemon]
-        print(f"Nombre: {datos['nombre']}\nTipo: {datos['tipo']}\nPeso: {datos['peso']}\n")
-    
-    # Elegir una clave aleatoria
-    clave_aleatoria = random.choice(list(pokemons.keys()))
-
-    # Obtener los datos del Pokémon aleatorio
-    pokemon_aleatorio = pokemons[clave_aleatoria]
-    
-    print(f"¡Has encontrado a {pokemon_aleatorio['nombre']}!")
-
-    n_pista = random.randint(0, 3)
-    print(pista.generar_pista(n_pista))
