975a4690ff0269e7737af6b50867ccb8b5cbfb3d
Ainé Figueroa <aine.xena.100@gmail.com>
2025-02-20 23:30:00 +0100

    codigo de agregado pokemons explicado

--- Pokedex.py
+++ Pokedex.py
@@ -4,30 +4,37 @@ import json
 # Además, añado los pokemons a un json para que se queden guardados una vez salgas del programa,
 #   de esta forma, la pokedex se mantiene guardada y puedes volver a acceder a ella en futuras partidas.
 class Pokedex:
-    pokemons_capturados: list[Pokemon]
+    registro = list
 
     def __init__(self):
-        self.pokemons_capturados = []
+        self.registro = []
 
     def generar_pokedex(self, nombre_usuario: str) -> None:
-        # Genero un json vacío para un usuario en concreto
+        # Genero un diccionario vacío
         pokedex = {}
-        
-        # Devuelvo el json vacío
-        with open(f'{nombre_usuario}_pokedex.json', 'w') as archivo:
-            pokedex = json.load(archivo)
 
-        return pokedex
+        # Registro el nombre de usuario para no sobreescribir datos
+        self.registro.append(nombre_usuario)
+
+        if nombre_usuario in self.registro:
+            return None
+        else:
+            # Cargo el diccionario vacío en un archivo json
+            # Si no existe el archivo, lo crea
+            with open(f'{nombre_usuario}_pokedex.json', 'w') as archivo:  # modo escritura
+                json.dump(pokedex, archivo, indent=4)
 
     def agregar_pokemon(self, pokemon: Pokemon, nombre_usuario: str) -> None:
         try:
-            with open(f'{nombre_usuario}_pokedex.json', 'r') as archivo:
+            # Cargo los datos del json en una variable
+            with open(f'{nombre_usuario}_pokedex.json', 'r') as archivo:  # modo lectura
                 pokedex = json.load(archivo)
         except FileNotFoundError:
             pokedex = self.generar_pokedex(nombre_usuario)
         
         nombre = pokemon.nombre
         if nombre in pokedex:
+            # Esto para no duplicar pokemons, si existe, se suma 1 a la cantidad
             pokedex[nombre]['cantidad'] += 1
         else:
             pokedex[pokemon.nombre] = {
@@ -37,5 +44,7 @@ class Pokedex:
                 'n_pokedex': pokemon.n_pokedex,
                 'cantidad': 1
             }
+        
+        # Cargo los datos en el archivo json
         with open(f'{nombre_usuario}_pokedex.json', 'w') as archivo:
             json.dump(pokedex, archivo, indent=4)
